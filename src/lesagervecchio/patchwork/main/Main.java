7ba845701ec8dfbc8f0aad62f053ba2e0e669b4b
paul_l <paul_l@paull-Laptop-14>
2022-12-02 18:28:21 +0100

    test

--- src/lesagervecchio/patchwork/main/Main.java
+++ src/lesagervecchio/patchwork/main/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world !");
 		var global = new GlobalPatches();
-		System.out.println("Test");
+		System.out.println("Allo?");
 	}
 }
