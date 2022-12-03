6b320095953325b29041fe676c8a5db8cf1ff683
paul_l <paul_l@paull-Laptop-14>
2022-12-03 12:58:05 +0100

    Substance DisplayService

--- src/lesagervecchio/patchwork/display/GraphicalDisplay.java
+++ src/lesagervecchio/patchwork/display/GraphicalDisplay.java
@@ -1,5 +1,16 @@
 package lesagervecchio.patchwork.display;
 
-public class GraphicalDisplay {
+import lesagervecchio.patchwork.board.PlayerBoard;
+import lesagervecchio.patchwork.patch.Patch;
 
+public final class GraphicalDisplay implements DisplayService{
+  @Override
+  public void drawPatch(Patch patch) {
+
+  }
+
+  @Override
+  public void drawPlayerBoard(PlayerBoard board) {
+
+  }
 }
